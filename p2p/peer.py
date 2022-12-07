0b9cbff4e04c6d331118da518232582bf05ec532
Nielsencu <nielsencugito@gmail.com>
2022-12-07 11:50:55 +0100

    add default port

--- p2p/peer.py
+++ p2p/peer.py
@@ -17,4 +17,7 @@ if __name__ == "__main__":
     app = web.Application()
     app.router.add_get("/", index)
     app.router.add_get("/client.js", javascript)
-    web.run_app(app, host="0.0.0.0", port=int(sys.argv[1]))
+    port = 5000
+    if len(sys.argv) > 1:
+        port = sys.argv[1]
+    web.run_app(app, host="0.0.0.0", port=port)
